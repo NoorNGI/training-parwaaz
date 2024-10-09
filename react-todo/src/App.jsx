008219c64b7a4b78f875fef932ad849060f7914f
noor_ali <noor.ali@nextgeni.com>
2024-10-09 12:21:34 +0500

    todo-app: initial commit

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <main className='min-h-[300px] bg-white w-[500px] rounded-3xl shadow-lg'>
+      <h1 className='text-slate-500'>hello </h1>
+    </main>
+  )
+}
+
+export default App
