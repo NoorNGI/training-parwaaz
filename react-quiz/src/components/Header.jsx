a0cfc556f8c85fd9a7c142f6cf0ca0e149cbc3f1
noor_ali <noor.ali@nextgeni.com>
2024-10-14 12:04:13 +0500

    reac-quiz: spin the logo

--- react-quiz/src/components/Header.jsx
+++ react-quiz/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { useQuiz } from "../context/QuizContext";
+
 function Header() {
+  const { status } = useQuiz();
+
   return (
     <header className="app-header">
-      <img src="logo512.png" alt="React logo" />
+      <img
+        src="logo512.png"
+        alt="React logo"
+        className={`${status === "active" ? "rotate" : ""}`}
+      />
       <h1>The React Quiz</h1>
     </header>
   );
