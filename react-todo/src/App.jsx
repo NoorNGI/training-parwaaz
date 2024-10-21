750e8b5f8afc7e8781ffabc49d23f27b0ebb6632
noor_ali <noor.ali@nextgeni.com>
2024-10-21 18:34:20 +0500

    react-todo: refactoring some code

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,6 +3,7 @@ import TodoApp from "./pages/TodoApp";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Other from "./pages/Other";
 import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Route index path="/" element={<Home />} />
       <Route path="/app" element={<TodoApp />} />
       <Route path="/other" element={<Other />} />
-      <Route path="*" element={<div>Page not found</div>} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
---
+++ react-todo/src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-white">Page Not Found :(</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
